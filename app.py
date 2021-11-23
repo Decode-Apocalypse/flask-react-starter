7f7bce4ba917aa307037c96b0495d612eebe4bbb
bryanchau <nchau1@student.gsu.edu>
2021-11-23 17:19:54 +0000

    Updated readme and set up heroku postgresl

--- app.py
+++ app.py
@@ -4,12 +4,36 @@ from flask_login.utils import login_required
 import os
 import json
 
+from dotenv import load_dotenv, find_dotenv
+from flask_login import (
+    login_user,
+    current_user,
+    LoginManager,
+    UserMixin,
+    login_required,
+)
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv(find_dotenv())
+
 
 app = flask.Flask(__name__, static_folder="./build/static")
 # This tells our Flask app to look at the results of `npm build` instead of the
 # actual files in /templates when we're looking for the index page file. This allows
 # us to load React code into a webpage. Look up create-react-app for more reading on
 # why this is necessary.
+
+db_url = os.getenv("DATABASE_URL")
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+# Gets rid of a warning
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = b"I am a secret key!"
+
+db = SQLAlchemy(app)
+
+
 bp = flask.Blueprint("bp", __name__, template_folder="./build")
 
 
